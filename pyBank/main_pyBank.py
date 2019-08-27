aa30acad901d799cc1af6065bb3d08e0fa50f567
Ziyanlu <53975576+Ziyanlu@users.noreply.github.com>
2019-08-26 19:21:15 -0500

    Add files via upload

--- pyBank/main_pyBank.py
+++ pyBank/main_pyBank.py
@@ -1,1 +1,23 @@
-print ("Hello pyBank!")
+import os
+import csv
+bank_csv = os.path.join("Resources", "budget_data.csv")
+with open(bank_csv, newline = "") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter = "," )
+    csvheader = next(csvreader)
+    
+    # row = [row for row in csvreader]
+    # print(row[1])
+    # print(f"Total Month: {len(row)}")
+    
+    # number= []
+    # for row in csvreader:
+    #     number.append (int(row[1]))
+    # print(sum(number))
+
+
+    number= 0
+    for row in csvreader:
+        number += int(row[1])
+    print(number)
+
+    
